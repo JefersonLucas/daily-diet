aa7ca02996b3c558770177c49fc9c9d84f904ad8
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-02-09 19:53:28 -0300

    feat: imported fonts, theme, and Home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+	useFonts,
+	NunitoSans_400Regular,
+	NunitoSans_700Bold,
+} from '@expo-google-fonts/nunito-sans'
+import { HomeScreen } from '@screens/home'
+import { Loading } from '@components/Loading'
+import { ThemeProvider } from 'styled-components/native'
+import theme from '@theme'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [fontsLoaded] = useFonts({
+		NunitoSans_400Regular,
+		NunitoSans_700Bold,
+	})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return (
+		<ThemeProvider theme={theme}>
+			<StatusBar style="auto" />
+			{fontsLoaded ? <HomeScreen /> : <Loading />}
+		</ThemeProvider>
+	)
+}
