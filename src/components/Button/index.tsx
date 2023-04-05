de62a1e11d1bde4557c5374bd02d7aa3f43bea27
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-04-05 19:26:08 -0300

    feat: created button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import { TouchableOpacityProps } from 'react-native'
+
+import { Typography } from '@components/Typography'
+
+import { Container } from './styles'
+
+export type ButtonType = { type?: 'solid' | 'outline'; isActive?: boolean }
+
+type ButtonProps = TouchableOpacityProps &
+	ButtonType & {
+		icon?: React.ReactNode
+		title: string
+	}
+
+export function Button({
+	type = 'solid',
+	isActive = true,
+	...props
+}: ButtonProps) {
+	return (
+		<Container type={type} isActive={isActive} {...props}>
+			{props.icon && props.icon}
+			<Typography
+				color={type === 'solid' ? 'white' : 'gray_1'}
+				style={{ marginHorizontal: 12 }}
+			>
+				{props.title}
+			</Typography>
+		</Container>
+	)
+}
