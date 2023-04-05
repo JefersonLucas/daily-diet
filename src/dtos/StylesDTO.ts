22520351f3e8cc89dc114074fe3eb47eb6052342
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-04-05 19:35:11 -0300

    feat: created styles dto

---
+++ src/dtos/StylesDTO.ts
@@ -0,0 +1,19 @@
+export type LineHeight = 'xs' | 'sm' | 'md' | 'lg'
+
+// prettier-ignore
+export type FontSize = 'body_xs' | 'body_sm' | 'body_md' | 'title_xs' | 'title_sm' | 'title_md' | 'title_lg'
+
+export type FontSizeBody = 'body_xs' | 'body_sm' | 'body_md'
+
+export type FontSizeTitle = 'title_xs' | 'title_sm' | 'title_md' | 'title_lg'
+
+export type Family = 'regular' | 'bold'
+
+// prettier-ignore
+export type Color = 'gray_1' | 'gray_2' | 'gray_3' | 'gray_4' | 'gray_5' | 'gray_6' | 'gray_7' | 'white' | 'red_dark' | 'red_mid' | 'red_light' | 'green_dark' | 'green_mid' | 'green_light'
+
+// prettier-ignore
+export type ColorsBase = 'gray_1' | 'gray_2' | 'gray_3' | 'gray_4' | 'gray_5' | 'gray_6' | 'gray_7' | 'white'
+
+// prettier-ignore
+export type ColorsBrand = 'red_dark' | 'red_mid' | 'red_light' | 'green_dark' | 'green_mid' | 'green_light'
