d267cab06241cea69074ca565de5b0849d459dd4
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-04-14 16:26:59 -0300

    chore: added dtos alias

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = function (api) {
 					alias: {
 						'@assets': './src/assets',
 						'@components': './src/components',
+						'@dtos': './src/dtos',
 						'@routes': './src/routes',
 						'@screens': './src/screens',
 						'@storage': './src/storage',
