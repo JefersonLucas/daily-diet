7d973c25c402f17badb107125d76c3a7d50638f4
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-04-14 16:46:32 -0300

    refactor: added navigation, statistics

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const Container = styled(SafeAreaView)`
 	flex: 1;
 	background-color: ${({ theme }) => theme.colors.base.gray_7};
 	padding: 0 24px;
